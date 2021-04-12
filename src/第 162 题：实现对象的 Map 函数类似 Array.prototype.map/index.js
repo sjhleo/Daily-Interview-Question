4415615ce77f1545fcc7043cb8e9f878d2a2d236
leo <better_leo@163.com>
2021-04-12 13:10:48 +0800

    :fish:  实现对象的 Map 函数类似 Array.prototype.map

---
+++ src/第 162 题：实现对象的 Map 函数类似 Array.prototype.map/index.js
@@ -0,0 +1,11 @@
+function map(obj, fn) {
+    let result = obj;
+    Object.keys(obj).forEach(function (key) {
+        let value = fn.call(null, obj[key], key, obj);
+        result[key] = value;
+    });
+    return result;
+}
+Object.prototype.map = function (fn) {
+    map.call(null, this, fn);
+};
