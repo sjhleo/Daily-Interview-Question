77005c58e3fd0fc22a7d5c637a1feefacf50d86f
leo <better_leo@163.com>
2021-04-21 20:53:51 +0800

    :star:  异步循环添加不同实现

--- src/输出以下代码运行结果，为什么？如果希望每隔 1s 输出一个结果，应该如何改造？注意不可改动 square 方法/index.js
+++ src/输出以下代码运行结果，为什么？如果希望每隔 1s 输出一个结果，应该如何改造？注意不可改动 square 方法/index.js
@@ -23,3 +23,19 @@ function test() {
         p = p.then(() => square(x).then(res => console.log(res)));
     });
 }
+// 这样写也可以
+function test() {
+    var p = Promise.resolve();
+    list.reduce((pre, current) => {
+        return pre.then(() => square(current).then(res => console.log(res)));
+    }, p);
+}
+function test() {
+    var p = Promise.resolve();
+    list.reduce(async (pre, current) => {
+        await pre;
+        let result = await square(current);
+        console.log(result);
+        return result;
+    }, p);
+}
